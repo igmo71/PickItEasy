a9062016cfafd0a7846a85c387bec9a2fd0cc886
Igor Mogilnitskiy <igmo71@outlook.com>
2023-03-30 18:12:08 +0300

    Send reqiest to 1c.

--- PickItEasy.WebApp.BlazorServer/Pages/WhsOrdersOut/Item.razor.cs
+++ PickItEasy.WebApp.BlazorServer/Pages/WhsOrdersOut/Item.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using PickItEasy.Application.Interfaces;
+using PickItEasy.Application.Interfaces.Integration;
 using PickItEasy.Domain.Entities;
 
 namespace PickItEasy.WebApp.BlazorServer.Pages.WhsOrdersOut
@@ -9,6 +10,9 @@ namespace PickItEasy.WebApp.BlazorServer.Pages.WhsOrdersOut
     {
         [Inject]
         public IApplicationDbContext? DbContext { get; set; }
+
+        [Inject]
+        public IClient1cUt? Client1cUt { get; set; }
 
         [Parameter]
         public string? Id { get; set; }
@@ -22,6 +26,9 @@ namespace PickItEasy.WebApp.BlazorServer.Pages.WhsOrdersOut
             whsOrderOut = await DbContext.WhsOrdersOut.FirstOrDefaultAsync(e => e.Id.ToString() == Id);
         }
 
-
+        private async Task PostWhsOrderOut()
+        {
+            await Client1cUt.PostWhsOrderOutAsync(whsOrderOut);
+        }
     }
 }
