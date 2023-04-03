3e9834baa17dd03b6ae39a790aa40446d33879c8
Igor Mogilnitskiy <igmo71@outlook.com>
2023-04-03 10:27:53 +0300

    Add Status and Queue.

---
+++ PickItEasy.Persistence/Data/EntityTypeConfigurations/WhsOrderOutQueueConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PickItEasy.Application.Services.WhsOrdersOut.Queries.GetById;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PickItEasy.Domain.Entities;
+using PickItEasy.Application.Common;
+
+namespace PickItEasy.Persistence.Data.EntityTypeConfigurations
+{
+    public class WhsOrderOutQueueConfiguration : IEntityTypeConfiguration<WhsOrderOutQueue>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<WhsOrderOutQueue> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Name).IsRequired()
+                .HasColumnType(EntityConfig.TYPE_VARCHAR).HasMaxLength(EntityConfig.MAX_LENGTH_NAME);
+        }
+    }
+}
---
+++ PickItEasy.Persistence/Data/EntityTypeConfigurations/WhsOrderOutStatusConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PickItEasy.Application.Common;
+using PickItEasy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickItEasy.Persistence.Data.EntityTypeConfigurations
+{
+    public class WhsOrderOutStatusConfiguration : IEntityTypeConfiguration<WhsOrderOutStatus>
+    {
+        public void Configure(EntityTypeBuilder<WhsOrderOutStatus> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Name).IsRequired()
+                .HasColumnType(EntityConfig.TYPE_VARCHAR).HasMaxLength(EntityConfig.MAX_LENGTH_NAME);
+        }
+    }
+}
