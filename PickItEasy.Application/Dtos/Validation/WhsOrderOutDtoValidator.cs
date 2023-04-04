64347e93dd79071d43ac490c2432ef7c83e5f42d
Igor Mogilnitskiy <igmo71@outlook.com>
2023-04-04 10:36:23 +0300

    Validator for order status.

--- PickItEasy.Application/Dtos/Validation/WhsOrderOutDtoValidator.cs
+++ PickItEasy.Application/Dtos/Validation/WhsOrderOutDtoValidator.cs
@@ -1,19 +1,24 @@
 ﻿using FluentValidation;
+using MediatR;
+using PickItEasy.Application.Services.WhsOrderOutStatuses.Queries.Contains;
 using PickItEasy.Domain.Entities;
 
 namespace PickItEasy.Application.Dtos.Validation
 {
     public class WhsOrderOutDtoValidator : AbstractValidator<WhsOrderOutDto>
     {
-        public WhsOrderOutDtoValidator()
+        private readonly IMediator _mediator;
+        public WhsOrderOutDtoValidator(IMediator mediator)
         {
+            _mediator = mediator;
             RuleFor(whsOrderOutDto => whsOrderOutDto.Id).NotNull().NotEmpty();
             RuleFor(whsOrderOutDto => whsOrderOutDto.Name).NotNull().NotEmpty();
             RuleFor(whsOrderOutDto => whsOrderOutDto.Number).NotNull().NotEmpty();
             RuleFor(whsOrderOutDto => whsOrderOutDto.DateTime).NotNull().NotEmpty();
             RuleFor(whsOrderOutDto => whsOrderOutDto.Products).NotNull().NotEmpty();
             RuleForEach(whsOrderOutDto => whsOrderOutDto.Products).SetValidator(new WhsOrderOutProductDtoValidator());
-            RuleFor(WhsOrderOutDto => WhsOrderOutDto.Status).NotNull().Must(status => PickItEasy.Integration.Connectors.Ut1c.WhsOrderOutStatus.Contain(status));
+            RuleFor(WhsOrderOutDto => WhsOrderOutDto.Status).NotNull()
+                .Must(status => _mediator.Send(new ContainsValueQuery { Value = status }).GetAwaiter().GetResult()); // TODO: Check!
         }
     }
 }
