983e2fa12e07711db99f818705a299242bd4796f
Igor Mogilnitskiy <igmo71@outlook.com>
2023-04-17 14:56:11 +0300

    Refactor.

--- PickItEasy.Domain/Entities/Catalog.cs
+++ PickItEasy.Domain/Entities/Catalog.cs
@@ -1,9 +1,4 @@
 ﻿using PickItEasy.Domain.Entities.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PickItEasy.Domain.Entities
 {
