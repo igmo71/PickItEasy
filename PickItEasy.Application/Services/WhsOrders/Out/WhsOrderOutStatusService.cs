6bec2724c5be504e1990d297a8dc914a93f49574
Igor Mogilnitskiy <igmo71@outlook.com>
2023-04-25 12:38:17 +0300

    WhsOrderOutStatusService.

---
+++ PickItEasy.Application/Services/WhsOrders/Out/WhsOrderOutStatusService.cs
@@ -0,0 +1,11 @@
+﻿using PickItEasy.Application.Interfaces;
+using PickItEasy.Application.Interfaces.WhsOrders.Out;
+using PickItEasy.Domain.Entities;
+
+namespace PickItEasy.Application.Services.WhsOrders
+{
+    public class WhsOrderStatusService : IWhsOrderStatusService
+    {
+        
+    }
+}
---
+++ PickItEasy.Application/Services/WhsOrders/Out/WhsOrderStatusDto.cs
@@ -0,0 +1,13 @@
+﻿using PickItEasy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickItEasy.Application.Services.WhsOrders.Out
+{
+    public class WhsOrderStatusDto : WhsOrderStatus
+    {
+    }
+}
