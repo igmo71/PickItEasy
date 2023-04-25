6d9f2609eea0c6c32e2daee8d80c7940cfea2d48
Igor Mogilnitskiy <igmo71@outlook.com>
2023-04-25 13:01:11 +0300

    WhsOrderOutQueueService.

---
+++ PickItEasy.Application/Interfaces/WhsOrders/Out/IWhsOrderOutStatusService.cs
@@ -0,0 +1,12 @@
+﻿using PickItEasy.Application.Services.WhsOrders.Out.Statuses;
+
+namespace PickItEasy.Application.Interfaces.WhsOrders.Out
+{
+    public interface IWhsOrderOutStatusService
+    {
+        Task<WhsOrderOutStatusVm> CreateAsync(WhsOrderOutStatusDto dto);
+        Task UpdateAsync(WhsOrderOutStatusDto dto);
+        Task<Guid> GetIdByValueAsync(int value);
+        Task<WhsOrderOutStatusListVm> GetListAsync();
+    }
+}
---
+++ PickItEasy.Application/Services/WhsOrders/Out/Queues/WhsOrderOutQueueDto.cs
@@ -0,0 +1,13 @@
+﻿using PickItEasy.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickItEasy.Application.Services.WhsOrders.Out.Queues
+{
+    public class WhsOrderOutQueueDto : WhsOrderOutQueue
+    {
+    }
+}
