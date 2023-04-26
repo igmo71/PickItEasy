50b4a16a096d3abd6b50da33a2d5eef44ec39153
Igor Mogilnitskiy <igmo71@outlook.com>
2023-04-26 16:09:07 +0300

    Add intefaces IProductService and IWarehouseService.

---
+++ PickItEasy.Application/Interfaces/Services/IWarehouseService.cs
@@ -0,0 +1,14 @@
+﻿using PickItEasy.Application.Models.Warehouses;
+
+namespace PickItEasy.Application.Interfaces.Services
+{
+    public interface IWarehouseService
+    {
+        Task<WarehouseDto> CreateAsync(WarehouseDto dto);
+        Task DeleteAsync(Guid id);
+        Task DisableAsync(Guid id);
+        Task<WarehouseVm> GetByIdAsync(Guid id);
+        Task<bool> IsExistsByIdAsync(Guid id);
+        Task UpdateAsync(WarehouseDto dto);
+    }
+}
